9e57c0c3065dbbf47bbfbc8e2b320d897ad8ff48
fortunatus <ihedfortune@gmail.com>
2024-01-08 07:33:09 +0100

    initial commit

---
+++ shoopers-client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/navigation/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ shoopers-client/src/assets/index.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png"
+import gadget1 from "./gadget1.jpeg"
+import gadget2 from "./gadget2.jpeg"
+import gadget3 from "./gadget3.jpeg"
+import gadget4 from "./gadget4.jpeg"
+import github from "./github-logo.png"
+import user from "./user.jpg"
+import payments from "./paymentlogos.jpeg"
+import cart from "./cart.jpeg"
+import google from "./google-logo.png"
+
+export {logo, gadget1, gadget2, gadget3,gadget4,github,user,payments,cart, google}
