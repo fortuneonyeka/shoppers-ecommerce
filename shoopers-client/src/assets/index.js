89738ed5c80bb6f59a52ddc38dc6a7a1433eda2b
fortunatus <ihedfortune@gmail.com>
2024-01-09 08:11:55 +0100

    display header items

--- shoopers-client/src/assets/index.js
+++ shoopers-client/src/assets/index.js
@@ -1,7 +1,7 @@
-import logo from "./logo.png"
+import Logo from "./logo.png"
 import gadget1 from "./gadget1.jpeg"
 import gadget2 from "./gadget2.jpeg"
-import gadget3 from "./gadget3.jpeg"
+import gadget3 from "./gadget3.png"
 import gadget4 from "./gadget4.jpeg"
 import github from "./github-logo.png"
 import user from "./user.jpg"
@@ -9,4 +9,4 @@ import payments from "./paymentlogos.jpeg"
 import cart from "./cart.jpeg"
 import google from "./google-logo.png"
 
-export {logo, gadget1, gadget2, gadget3,gadget4,github,user,payments,cart, google}
+export  {Logo, gadget1, gadget2,gadget3,gadget4,github,user,payments,cart, google}
--- shoopers-client/src/components/navigation/Header.jsx
+++ shoopers-client/src/components/navigation/Header.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Logo} from "../../assets/index.js"
 
 const Header = () => {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/home"
+    },
+    {
+      name: "About",
+      link: "/about"
+    },
+    {
+      name: "Shop",
+      link: "/shop"
+    },
+    {
+      name: "Blogs",
+      link: "/blogs"
+    }
+  ]
   return (
-    <div>Header</div>
+    <div className='w-full h-20 bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'>
+      <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between '>
+        <div className='w-10 h-10'>
+          <img className='rounded-full' src={Logo} alt="logo " />
+        </div>
+      <div className='flex items-center gap-6 text-xl'>
+      {navItems.map((item, index) => {
+          return <ul key={index}><li className='cursor-pointer'>{item.name}</li></ul>
+          })}
+      </div>
+      </div>
+    </div>
   )
 }
 
