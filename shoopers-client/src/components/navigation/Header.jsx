e6fae966d7c4aa6c8938953b4ef60cc5011e9039
fortunatus <ihedfortune@gmail.com>
2024-01-10 07:21:31 +0100

    style navbar

--- shoopers-client/src/components/navigation/Header.jsx
+++ shoopers-client/src/components/navigation/Header.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {Logo} from "../../assets/index.js"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Logo } from '../../assets/index.js';
 
 const Header = () => {
   const navItems = [
     {
-      name: "Home",
-      link: "/home"
+      name: 'Home',
+      link: '/home',
     },
     {
-      name: "About",
-      link: "/about"
+      name: 'About',
+      link: '/about',
     },
     {
-      name: "Shop",
-      link: "/shop"
+      name: 'Shop',
+      link: '/shop',
     },
     {
-      name: "Blogs",
-      link: "/blogs"
-    }
-  ]
+      name: 'Blogs',
+      link: '/blogs',
+    },
+  ];
+
   return (
     <div className='w-full h-20 bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'>
       <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between '>
         <div className='w-10 h-10'>
-          <img className='rounded-full' src={Logo} alt="logo " />
+          <img className='rounded-full' src={Logo} alt='logo' />
         </div>
-      <div className='flex items-center gap-6 text-xl'>
-      {navItems.map((item, index) => {
-          return <ul key={index}><li className='cursor-pointer'>{item.name}</li></ul>
+        <div className='flex items-center gap-8 text-xl'>
+          {navItems.map((item, index) => {
+            return <div key={index}><p className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>{item.name}</p></div>
           })}
-      </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
