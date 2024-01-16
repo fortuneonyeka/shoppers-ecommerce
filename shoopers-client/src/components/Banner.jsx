7603fe2d5b3ad45ed7851305e3fe0948be4d7104
fortunatus <ihedfortune@gmail.com>
2024-01-16 07:56:38 +0100

    hide overflow-x

--- shoopers-client/src/components/Banner.jsx
+++ shoopers-client/src/components/Banner.jsx
@@ -22,7 +22,7 @@ const Banner = () => {
   };
 
   return (
-    <div className="w-full h-auto ">
+    <div className="w-full h-auto overflow-x-hidden">
       <div className="w-screen h-[650px] relative">
         <div className="w-[400vw] h-full flex gap-">
           <img
