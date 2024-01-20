3142705e8f9b2a779a2e6566b26265449062b4c0
fortunatus <ihedfortune@gmail.com>
2024-01-20 12:03:26 +0100

    fetch api

--- shoopers-client/src/components/ProductCard.jsx
+++ shoopers-client/src/components/ProductCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
+  const truncatedTitle = product.title.length > 20 ? product.title.slice(0, 20) + '...' : product.title;
   return (
-    <div>ProductCard</div>
+    <div className=''>
+      <div className='flex flex-col gap-4 p-4 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] rounded-lg w-full h-full cursor-pointer overflow-hidden group'>
+        <img src={product.image} alt="" className='shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] rounded-xl px-4 h-full w-full object-cover group-hover:scale-110 duration-500'/>
+        <div className='text-sm'>
+          <p className='text-lg font-bold'>{truncatedTitle}</p>
+          <div className='flex flex-co gap-2'>
+          <p className='group-hover:hidden'>Price: </p>
+      <p className="line-through px-2 group-hover:hidden">{product.oldPrice}</p>
+      <p className="group-hover:hidden">{product.price}</p>
+      <span className="hidden group-hover:flex text-green-400">Add to Cart</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- shoopers-client/src/components/Products.jsx
+++ shoopers-client/src/components/Products.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import ProductCart from "./ProductCard";
+import ProductCard from "./ProductCard";
 
-const Products = () => {
+const Products = ({products}) => {
   return (
-    <div className="py-10">
-      <div className="flex flex-col justify-center mx- items-center gap-4">
+    <div className="py-10 ">
+      <div className="flex flex-col justify-center mx- items-center gap-4 ">
             <h1 className="text-2xl bg-gray-800 text-white py-4 px-6 w-85 text-center rounded-md">Elevate Your Shopping Experience: Where Quality Meets Convenience</h1>
             <span className="w-20 h-[5px] bg-black"></span>
             <p className="max-w-[700px] text-gray-600 text-center">Discover, Shop, and Elevate Your Style – Where Every Click is a Fashion Statement! <strong className="text-orange-600 text-lg">Shop Smarter, Live Better.</strong></p>
       </div>
-      <div className="max-w-screen-xl mx-auto">
-            <ProductCart />
+      <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-2 gap-10 md:grid-cols-4 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] px-6 rounded-md">
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product}/>
+            ))}
       </div>
     </div>
   );
