1f32f084282db2b6bc7b02ac7561d9c05d1ab3b9
fortunatus <ihedfortune@gmail.com>
2024-02-06 08:01:14 +0100

    setup redux store and slice

---
+++ shoopers-client/src/features/CartSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+      value:0
+}
+
+export const cartSlice = createSlice({
+      name: "product",
+      initialState,
+      reducers: {
+            
+      }
+})
