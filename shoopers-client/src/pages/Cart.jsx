e00c2f02c789128242a90c95a2adc7ede1cf3d48
fortunatus <ihedfortune@gmail.com>
2024-02-11 13:17:06 +0100

    dispatch decreamentItem and increamentItem

--- shoopers-client/src/pages/Cart.jsx
+++ shoopers-client/src/pages/Cart.jsx
@@ -1,14 +1,14 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrementItem, deleteItem, resetCart } from "../features/shopperSlice";
+import { decrementItem, deleteItem, resetCart, incrementItem } from "../features/shopperSlice";
 import { MdDeleteForever } from "react-icons/md";
 import { RxReset } from "react-icons/rx";
 import { CiBatteryEmpty } from "react-icons/ci";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.shopper.productData);
- 
   const dispatch = useDispatch();
+
   // Calculate total price using useMemo to avoid unnecessary re-renders
   const totalPrice = useMemo(() => {
     return cartItems.reduce((total, item) => {
@@ -20,10 +20,10 @@ const Cart = () => {
     <div>
       <div className="flex flex-col py-12">
         {cartItems.length > 0 && (
-          <div className=" text-xl font-bold px-12 flex gap-4 justify-end">
-            <p> Total Price: ${totalPrice.toFixed(2)}</p>
-            <div onClick={() =>dispatch(resetCart())} className="text-red-300 cursor-pointer relative group">
-              <RxReset  />
+          <div className="text-xl font-bold px-12 flex gap-4 justify-end">
+            <p>Total Price: ${totalPrice.toFixed(2)}</p>
+            <div onClick={() => dispatch(resetCart())} className="text-red-300 cursor-pointer relative group">
+              <RxReset />
               <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute bottom-6 right-0 text-xs text-red-500">
                 clear cart
               </p>
@@ -37,7 +37,7 @@ const Cart = () => {
                 key={item._id}
                 className="max-w-screen-xl mx-auto flex gap-16 hover:scale-95 duration-500 relative"
               >
-                <div className=" flex gap-6 px-9 rounded-lg relative shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] justify-center items-center">
+                <div className="flex gap-6 px-9 rounded-lg relative shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] justify-center items-center">
                   <img
                     className="w-[40%] h-[300px] object-fit"
                     src={item.image}
@@ -50,8 +50,8 @@ const Cart = () => {
                     <p>Price: ${item.quantity * item.price}</p>
                   </div>
 
-                  <p className="absolute left-2 top-2 text-2xl cursor-pointer">-</p>
-                  <p onClick={() => dispatch(decrementItem())} className="absolute left-8 top-2 text-2xl cursor-pointer">+</p>
+                  <p onClick={() => dispatch(decrementItem({ _id: item._id }))} className="absolute left-2 top-2 text-2xl cursor-pointer">-</p>
+                  <p onClick={() => dispatch(incrementItem({ _id: item._id }))} className="absolute left-8 top-2 text-2xl cursor-pointer">+</p>
                   
                   <p
                     onClick={() => dispatch(deleteItem(item._id))}
@@ -64,11 +64,10 @@ const Cart = () => {
             ))
           ) : (
             <div className="flex flex-col relative text-red-300 ">
-
-            <p className="text-4xl h-[70vh] flex justify-center text-center">
-              You have no Item in your cart
-            </p>
-            <p className="text-black text-4xl absolute top-12 left-28 font-light"><CiBatteryEmpty className="text-[200px] font-light text-red-300 " /></p>
+              <p className="text-4xl h-[70vh] flex justify-center text-center">
+                You have no Item in your cart
+              </p>
+              <p className="text-black text-4xl absolute top-12 left-28 font-light"><CiBatteryEmpty className="text-[200px] font-light text-red-300 " /></p>
             </div>
           )}
         </div>
