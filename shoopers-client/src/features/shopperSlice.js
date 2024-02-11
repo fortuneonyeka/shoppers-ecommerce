0914435dc32dd78c30cab6814c6eeb93cfb646c7
fortunatus <ihedfortune@gmail.com>
2024-02-11 12:30:11 +0100

    add delete and rest reducers

--- shoopers-client/src/features/shopperSlice.js
+++ shoopers-client/src/features/shopperSlice.js
@@ -19,9 +19,25 @@ const shopperSlice = createSlice({
         state.productData.push(action.payload);
       }
     },
+    decrementItem: (state, action) => {
+     const item = state.productData.find((item) => item._id === action.payload)
+     if (item.quantity === 1) {
+      item.quantity =1
+     }else {
+      item.quantity--
+     }
+    },
+
+    deleteItem: (state, action) => {
+      state.productData = state.productData.filter((item) => item._id !== action.payload)
+    },
+    resetCart: (state) => {
+      state.productData = []
+    }
+
   },
 });
 
-export const { addToCart } = shopperSlice.actions;
+export const { addToCart, deleteItem, resetCart, decrementItem } = shopperSlice.actions;
 
 export default shopperSlice.reducer;
