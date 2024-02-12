08899348a2b4d32c27764e5ac36b6d5ea4bf10ee
fortunatus <ihedfortune@gmail.com>
2024-02-12 07:57:14 +0100

    update footer

--- shoopers-client/src/components/navigation/Footer.jsx
+++ shoopers-client/src/components/navigation/Footer.jsx
@@ -34,14 +34,14 @@ const Footer = () => {
   ];
 
   return (
-    <div className='bg-gray-800 text-[#949494] font-titleFont px-4 bottom-0 z-50 '>
-      <div className='max-w-screen-xl mx-auto grid grid-cols-2 gap-2 md:flex md:flex-row py-5 justify-between'>
+    <div className='bg-gray-800 text-[#949494] font-titleFont px-4 bottom-0 z-50 fixed w-[100%] h-[12%] '>
+      <div className='max-w-screen-xl mx-auto grid grid-cols-2 gap- md:flex md:flex-row py- mb-20 justify-between'>
          
-        <div className='w-10 h-10 flex flex-col gap-6'>
+        <div className='w-10 h-10 flex flex-col gap- py-4'>
           <Link to="/"><img className='rounded-full cursor-pointer hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)]' src={Logo} alt='Logo' /></Link>
           <p>@Ihedoro</p>
         </div>
-        <div className='flex md:flex-col gap-6 py-8 pt-14 px-8'>
+        <div className='flex md:flex-col gap-6 py-3 px-8'>
           <div className='w-24 transform transition-transform duration-300 hover:scale-150'>
             <img src={payments} alt='payments' />
           </div>
@@ -57,14 +57,14 @@ const Footer = () => {
           </div>
         </div>
         
-        <div className='flex flex-col gap-2'>
-          <h3 className='text-md font-bold'>Contact Us</h3>
-          <p className='text-xs'>Victorial: Island, Lagos</p>
+        <div className='flex flex-col gap-2 py-3'>
+          <h3 className='text-md '>Contact Us</h3>
+          {/* <p className='text-xs'>Victorial: Island, Lagos</p> */}
           <p className='text-xs'>Mobile: +2348038852544</p>
           <p className='text-xs'>Email: ihedfortune@gmail.com</p>
-          <p className='text-xs'>Whatsapp: +2348038852544</p>
+          {/* <p className='text-xs'>Whatsapp: +2348038852544</p> */}
         </div>
-        <div className='flex flex-col gap-2 cursor-pointer'>
+        <div className='flex flex-col gap-1 py-2 cursor-pointer'>
           <h3 className='text-md font-bold'>Profile</h3>
           <a href='/account' className='text-xs flex gap-1 items-center transition-transform duration-300 hover:scale-150 hover:text-green-200'>
             <span><TiUser /></span>
@@ -83,11 +83,11 @@ const Footer = () => {
             Help & Support
           </a>
         </div>
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col py-2'>
             <p>Subscribe</p>
           <form action='' className='flex gap-2 md:gap-2 md:flex-col shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] rounded-md'>
-            <input type='email' placeholder='email' className='rounded-md py-2 px-1' />
-            <button className='bg-gray-700 text-white rounded-lg hover:bg-[#17A349] hover:text-black py-2 px-4 md:px-1 focus:border-none text-md'>
+            <input type='email' placeholder='email' className='rounded-md py-1 px-1' />
+            <button className='bg-gray-700 text-white rounded-lg hover:bg-[#17A349] hover:text-black py-1 px-4 md:px-1 focus:border-none text-md'>
               Subscribe
             </button>
           </form>
