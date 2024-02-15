79ba4a278108bed531bc555718f5cc901f10eb80
fortunatus <ihedfortune@gmail.com>
2024-02-15 08:13:42 +0100

    add back to shopping button with styling

--- shoopers-client/src/pages/Cart.jsx
+++ shoopers-client/src/pages/Cart.jsx
@@ -1,12 +1,19 @@
 import React, { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrementItem, deleteItem, resetCart, incrementItem } from "../features/shopperSlice";
+import {
+  decrementItem,
+  deleteItem,
+  resetCart,
+  incrementItem,
+} from "../features/shopperSlice";
 import { MdDeleteForever } from "react-icons/md";
 import { RxReset } from "react-icons/rx";
-import emptyCart from "../assets/empty-cart.avif"
+import emptyCart from "../assets/empty-cart.avif";
 import { FaPlusCircle } from "react-icons/fa";
 import { FaCircleMinus } from "react-icons/fa6";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
+import { IoPlayBack } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.shopper.productData);
@@ -20,27 +27,37 @@ const Cart = () => {
   }, [cartItems]);
 
   return (
-    
     <div className="h-full py-4">
       <div className="flex flex-col">
         {cartItems.length > 0 && (
-          <div className="flex justify-end px-6 items-center">
-          <div className="text-xl font-bold px-12 flex gap-4 justify-end  py-6">
-            <p>Total Price: ${totalPrice.toFixed(2)}</p>
-            <div onClick={() => dispatch(resetCart())& toast.error(
-                `You have cleared your cart`,
-                {
-                  position: "top-right",
+          <div className="flex justify-between px-6 items-center">
+            <p className="relative cursor-pointer group">
+             <IoPlayBack className="cursor-pointer" />
+            <Link to="/"><span className=" opacity-0 group-hover:opacity-100 absolute top-0 left-0 bg-green-400 text-black capitalize py-1 px-4 rounded-md text-xs shadow-[5px_5px_0px_0px_rgba(109,40,217)]">
+              Back to shopping
+              </span></Link>  
+            </p>
+            <div className="text-xl font-bold px-12 flex gap-6  py-6">
+              <p>Total Price: ${totalPrice.toFixed(2)}</p>
+              <div
+                onClick={() =>
+                  dispatch(resetCart()) &
+                  toast.error(`You have cleared your cart`, {
+                    position: "top-right",
+                  })
                 }
-              )} className="text-red-300 cursor-pointer relative group">
-              <RxReset />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute bottom-6 right-0 text-xs text-red-500">
-                clear cart
-              </p>
+                className="text-red-300 cursor-pointer relative group text-2xl"
+              >
+                <RxReset />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute bottom-6 right-0 text-xs text-red-500">
+                  clear cart
+                </p>
+              </div>
             </div>
-          </div>
-          
-          <button className=" text-white bg-black h-12 rounded-lg hover:bg-green-400 hover:text-black hover:capitalize hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] w-[200px]">check out</button>
+
+            <button className=" text-white bg-black h-12 rounded-lg hover:bg-green-400 hover:text-black hover:capitalize hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] w-[200px]">
+              check out
+            </button>
           </div>
         )}
         <div className="py-4 px-6 grid grid-cols-3 gap-6">
@@ -62,19 +79,30 @@ const Cart = () => {
                     <p>Quantity: {item.quantity}</p>
                     <p>Price: ${item.quantity * item.price}</p>
                   </div>
-                  
 
-                  <p onClick={() => dispatch(decrementItem({ _id: item._id }))} className="absolute left-2 top-2 text-2xl cursor-pointer"><FaCircleMinus className="hover:text-red-300"/></p>
-                  <p onClick={() => dispatch(incrementItem({ _id: item._id }))} className="absolute left-8 top-2 text-2xl cursor-pointer px-4"><FaPlusCircle className="hover:text-blue-600"/></p>
-                  
-                 
                   <p
-                    onClick={() => dispatch(deleteItem(item._id))& toast.error(
-                      `You have successfully removed ${item.title} from your cart`,
-                      {
-                        position: "top-right",
-                      }
-                    )}
+                    onClick={() => dispatch(decrementItem({ _id: item._id }))}
+                    className="absolute left-2 top-2 text-2xl cursor-pointer"
+                  >
+                    <FaCircleMinus className="hover:text-red-300" />
+                  </p>
+                  <p
+                    onClick={() => dispatch(incrementItem({ _id: item._id }))}
+                    className="absolute left-8 top-2 text-2xl cursor-pointer px-4"
+                  >
+                    <FaPlusCircle className="hover:text-blue-600" />
+                  </p>
+
+                  <p
+                    onClick={() =>
+                      dispatch(deleteItem(item._id)) &
+                      toast.error(
+                        `You have successfully removed ${item.title} from your cart`,
+                        {
+                          position: "top-right",
+                        }
+                      )
+                    }
                     className="absolute top-0 right-2 p-4 text-2xl cursor-pointer text-red-300"
                   >
                     <MdDeleteForever />
@@ -84,14 +112,19 @@ const Cart = () => {
             ))
           ) : (
             <div className="flex flex-col justify-center items-center text-red-300 w-full h-screen">
-              <p className="text-4xl text-center">You have no Item in your cart</p>
-              <img className="w-[800px] h-[800px]" src={emptyCart} alt="empty Cart" />
+              <p className="text-4xl text-center">
+                You have no Item in your cart
+              </p>
+              <img
+                className="w-[800px] h-[800px]"
+                src={emptyCart}
+                alt="empty Cart"
+              />
             </div>
           )}
         </div>
       </div>
     </div>
-   
   );
 };
 
