edca6b4bc84ff448eaa8c88e7b61d9558b5b997c
fortunatus <ihedfortune@gmail.com>
2024-02-22 07:59:22 +0100

    use try catch in api fetch

--- shoopers-client/src/api/Api.js
+++ shoopers-client/src/api/Api.js
@@ -1,6 +1,13 @@
 import axios from "axios"
 
 export async function productData(){
-      const products = await axios.get("https://fakestoreapiserver.reactbd.com/walmart");
-      return products
+      // const products = await axios.get("https://fakestoreapiserver.reactbd.com/walmart");
+      // return products
+      try {
+            const products = await axios.get("https://fakestoreapiserver.reactbd.com/walmart");
+            const data = products.data
+      return data
+      } catch (error) {
+            console.log(error);
+      }
 }
