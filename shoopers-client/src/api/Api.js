5169958f5b1c055840e9bd5c65de98338b3596ca
fortunatus <ihedfortune@gmail.com>
2024-02-27 08:05:13 +0100

    install ReactLoading

--- shoopers-client/src/api/Api.js
+++ shoopers-client/src/api/Api.js
@@ -2,9 +2,6 @@ import axios from "axios"
 
 
 export async function productData(){
-      
-      // const products = await axios.get("https://fakestoreapiserver.reactbd.com/walmart");
-      // return products
       try {
             const products = await axios.get("https://fakestoreapiserver.reactbd.com/walmart");
             const data = products.data
