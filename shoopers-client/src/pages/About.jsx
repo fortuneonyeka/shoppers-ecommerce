eed2925930c39d3d2dbf84ad22a7611533ed6291
fortunatus <ihedfortune@gmail.com>
2024-03-11 13:55:46 +0100

    add about page template

---
+++ shoopers-client/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='text-2xl mx-auto w-[60%] py-32'>
+      <p>Shoppers, an innovative ecommerce store, offers a seamless shopping experience for diverse needs. With a user-friendly interface and comprehensive product range, Shoppers caters to various preferences, from fashion to electronics and beyond. The platform prioritizes customer satisfaction, ensuring secure transactions and efficient delivery services. Shoppers fosters trust through transparent pricing and reliable customer support, empowering users to make informed decisions. Its dynamic features, including personalized recommendations and exclusive deals, enhance the shopping journey. Embracing modern trends, Shoppers integrates social shopping elements, fostering community engagement and user interaction. With its commitment to quality and convenience, Shoppers redefines online shopping, making it a preferred destination for discerning consumers.</p>
+    </div>
+  )
+}
