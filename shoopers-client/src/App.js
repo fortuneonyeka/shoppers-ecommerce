caaf3af5c67f4480dd20f50b2df142fe8ab7962d
fortunatus <ihedfortune@gmail.com>
2024-03-12 07:58:44 +0100

    update routes

--- shoopers-client/src/App.js
+++ shoopers-client/src/App.js
@@ -14,6 +14,7 @@ import {
   ScrollRestoration,
 } from "react-router-dom";
 import { About } from "./pages/About";
+import Login from "./pages/Login";
 
 const Layout = () => {
   return (
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
       {
         path:"/about",
         element: <About />
+      },
+      {
+        path:"/login",
+        element: <Login />
       },
       {
         path:"*",
