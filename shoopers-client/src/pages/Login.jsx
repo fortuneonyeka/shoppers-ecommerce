d8cc516e7a5fdef89fd9dbe4361151900727cdf9
fortunatus <ihedfortune@gmail.com>
2024-03-17 09:29:23 +0100

    add logout button  and update button styling

--- shoopers-client/src/pages/Login.jsx
+++ shoopers-client/src/pages/Login.jsx
@@ -53,14 +53,25 @@ const Login = () => {
             type="submit"
             className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] w-[30%] py-3 rounded-lg flex justify-center mx-auto text-xl font-medium hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] bg-blue-600 text-white hover:bg-green-500 hover:text-black"
           >
-            Submit
+            Login
           </button>
         </form>
       </div>
-      <div className="w-14 h-10 flex gap-20 absolute bottom-[30%] left-[45%]">
-        <img src={Google} alt="google" className="rounded-xl hover:scale-125 duration-100 cursor-pointer" />
+      <div className="w-19 h-14 flex gap-20 absolute bottom-[32%] left-[40%]">
+        <img
+          src={Google}
+          alt="google"
+          className="rounded-xl hover:scale-125 duration-100 cursor-pointer"
+        />
 
-        <img src={Github} alt="github" className="rounded-xl hover:scale-125 duration-100 cursor-pointer" />
+        <img
+          src={Github}
+          alt="github"
+          className="rounded-xl hover:scale-125 duration-100 cursor-pointer"
+        />
+        {/* <button className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] w-[30%] px-4 rounded-lg flex text-center justify-center items-center mx-auto text-xl font-medium hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] bg-black text-white hover:bg-orange-500 hover:text-black">
+          Sign Out
+        </button> */}
       </div>
       <div></div>
     </div>
