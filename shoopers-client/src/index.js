01c140c2c80c7b50f818e7c91eb1cad9d64c9e61
fortunatus <ihedfortune@gmail.com>
2024-03-20 07:25:49 +0100

    add firebase app

--- shoopers-client/src/index.js
+++ shoopers-client/src/index.js
@@ -6,13 +6,14 @@ import { store, persistor } from "./features/store";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 import { PersistGate } from 'redux-persist/integration/react'
+import { app } from "./firebase.config";
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider store={store} app={app}>
      <PersistGate loading={null} persistor={persistor}>
     <App />
     </PersistGate>
