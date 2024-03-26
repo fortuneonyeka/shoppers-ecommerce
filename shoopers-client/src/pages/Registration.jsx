b895c406d9081dc9678d5aaef70bb060fb3b0b50
fortunatus <ihedfortune@gmail.com>
2024-03-26 08:03:51 +0100

    registration logic

--- shoopers-client/src/pages/Registration.jsx
+++ shoopers-client/src/pages/Registration.jsx
@@ -1,27 +1,69 @@
 import React,{ useState } from 'react'
+import { doCreateUserWithEmailAndPassword } from '../firebase/auth';
+import { useAuth } from '../contexts/authContext';
+import { Navigate } from "react-router-dom"; 
+
 
 const Registration = () => {
-      const initialState = {
-            email: "",
-            password: "",
-          };
+  const  userLoggedIn  = useAuth();
+      // const initialState = {
+      //       email: "",
+      //       password: "",
+      //       confirmPassword: ""
+      //     };
         
-          const [details, setDetails] = useState(initialState);
+      //     const [details, setDetails] = useState(initialState);
+      //     const [isRegistering, setIsRegistering] = useState(false)
+      //     const [errorMessage, setErrorMessage] = useState("")
         
-          const handleChange = (e) => {
-            const { name, value } = e.target;
-            setDetails((prev) => ({ ...prev, [name]: value }));
-          };
+      //     const handleChange = (e) => {
+      //       const { name, value } = e.target;
+      //       setDetails((prev) => ({ ...prev, [name]: value }));
+      //     };
         
-          const handleSubmit = (e) => {
-            e.preventDefault();
-            console.log(details);
-            setDetails(initialState);
-          };
+      //     const handleSubmit = async (e) => {
+      //       e.preventDefault();
+      //       if (!isRegistering) {
+      //         setIsRegistering(true)
+      //         await doCreateUserWithEmailAndPassword(details)
+      //       }
+      //       setDetails(initialState);
+      //     };
+      const [isRegistering, setIsRegistering] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isRegistering) {
+      setIsRegistering(true)
+      try {
+        // Ensure email is a string before passing it to createUserWithEmailAndPassword
+        if (typeof email === 'string' && email.trim() !== '') {
+          await doCreateUserWithEmailAndPassword(email, password);
+          // Registration successful, you can redirect the user to another page
+        } else {
+          throw new Error('Invalid email format');
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+  
+      setLoading(false);
+    };
+    }
+
+
 
 
   return (
       <div className="h-screen relative py-12">
+        {userLoggedIn && (<Navigate to={'/home'} replace={true}/>)}
       <div className="py-[25vh] flex  justify-center px-4">
         <form
           onSubmit={handleSubmit}
@@ -30,31 +72,42 @@ const Registration = () => {
         >
           <p className="text-center text-2xl text-white">Sign Up</p>
           <input
-            onChange={handleChange}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
-            value={details.email}
+            value={email}
             name="email"
             id=""
             placeholder="Enter your email"
+            required
             className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] px-4 py-3 rounded-lg"
           />
           <input
-            onChange={handleChange}
+            onChange={(e) =>setPassword(e.target.value) }
             type="password"
-            value={details.password}
+            value={password}
             placeholder="password"
+            required
             name="password"
             id=""
             className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] px-4 py-3 rounded-lg"
           />
+          {/* <input
+            onChange={handleChange}
+            type="password"
+            value={details.confirmPassword}
+            placeholder="confirm password"
+            name="password"
+            id=""
+            className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] px-4 py-3 rounded-lg"
+          /> */}
           <button
-            type="submit"
-            className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] w-[30%] py-3 rounded-lg flex justify-center mx-auto lg:text-xl font-medium hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] bg-blue-600 text-white hover:bg-green-500 hover:text-black"
+            type="submit" disabled={loading}
+            className="shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] w-[30%] cursor-pointer py-3 rounded-lg flex justify-center mx-auto lg:text-xl font-medium hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] bg-blue-600 text-white hover:bg-green-500 hover:text-black"
           >
             Sign Up
           </button>
         </form>
-     
+        {error && <p>{error}</p>}
       </div>
       <div></div>
     </div>
