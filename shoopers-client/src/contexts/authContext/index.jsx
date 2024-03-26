f04a43c622a1fb127dd64ba60f5d18630d0f76ba
fortunatus <ihedfortune@gmail.com>
2024-03-26 08:04:12 +0100

    refactor

--- shoopers-client/src/contexts/authContext/index.jsx
+++ shoopers-client/src/contexts/authContext/index.jsx
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {createContext, useContext, useEffect, useState } from 'react'
 import { auth } from '../../firebase/firebase'
 import { onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth' 
 
-const AuthContext = React.createContext()
+
+export const AuthContext = createContext();
 
 export function useAuth() {
-      return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({children}) {
